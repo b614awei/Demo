4b7c3bfab9c194aa9c783c60ea5a1d3eea0d103b
b614awei <b614awei@163.com>
2012-11-29 16:02:51 +0800

    fdg

---
+++ wus/wus/WILLAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WILLAppDelegate.h
+//  wus
+//
+//  Created by Ibokan on 12-11-29.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WILLAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
